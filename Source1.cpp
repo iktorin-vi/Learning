06bdec8446d00fbab310685b1deee487d1371577
iktorin-vi <nk.akk16@gmail.com>
2024-11-10 21:05:14 +0200

    d

---
+++ Source1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <Windows.h>
+using namespace std;
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    srand(static_cast<unsigned int>(time(nullptr))); // Ініціалізація генератора випадкових чисел
+    int secretNumber = rand() % 100 + 1; // Випадкове число від 1 до 100
+    int guess;
+    int attempts = 0;
+
+    cout << "Гра: Вгадай число від 1 до 100!" << endl;
+
+    // Головний цикл гри
+    while (true) {
+        cout << "Введіть вашу відповідь: ";
+        cin >> guess;
+        attempts++;
+
+        if (guess == secretNumber) {
+            cout << "Вітаємо! Ви вгадали число за " << attempts << " спроб." << endl;
+            break;
+        }
+        else if (guess < secretNumber) {
+            cout << "Загадане число більше. Спробуйте ще раз." << endl;
+        }
+        else {
+            cout << "Загадане число менше. Спробуйте ще раз." << endl;
+        }
+    }
+
+    cout << "Дякуємо за гру!" << endl;
+    return 0;
+}
